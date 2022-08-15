ceb6f860925018a6b8a7ceadb7c83ac376d6c24a
Gaurab Kumar <progaurab@gmail.com>
2022-08-15 11:44:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react-capacitor',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
